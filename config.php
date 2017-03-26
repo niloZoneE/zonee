b29678aa9f154710766289a040d3c4fbab119dad
yannick <yannick.elusue@hotmail.fr>
2017-03-26 15:00:46 +0200

    addPage

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $PARAM_hote        = "localhost";
 $PARAM_nom_bd      = "zone_entrepreneurs";
 $PARAM_utilisateur = "root";
-$PARAM_mot_passe   = "root";
+$PARAM_mot_passe   = "administrateur";
 $PARAM_url_site	   = "www.zone-entrepreneurs.fr";
 $PARAM_mail_site   = "zone.e@zone-entrepreneurs.fr";
 $PARAM_nom_site    = "zone-entrepreneurs";
---
+++ tarifs.php
@@ -0,0 +1,88 @@
+<?php session_start();
+include('header.html');
+?>
+  <div class="container-fluid">
+  <center>
+    <div class="col-md-7">
+      <h2>COIX DES TARIFS</h2>
+      <p>fdghjvmnbvfgdhfj fghdng hbfsdghmnbg gdfn dhrtfghj tghrsdgj ghrstdgjf gerfhs ter</p>
+    </div>
+  </center>
+  </div>
+<div class="container-fluid" style="background-color: #d5d7d8;">
+<div class="container">
+	<div class="row">
+		<div class="card text-center" style="width: 20rem;margin: 10px;">
+			  <div class="card-header">
+			    Featured
+			  </div>
+		  <div class="card-block">
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		  </div>
+		  <div class="card-block">
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		  </div>
+		  <ul class="list-group list-group-flush">
+		    <li class="list-group-item">Cras justo odiodbgxxzb strehgrthrth</li>
+		    <li class="list-group-item">Dapibus ac facilisis in terhearthgert heth eth</li>
+		    <li class="list-group-item">Vestibulum at erosh etherth th therter hyehehte </li>
+		    <li class="list-group-item">Cras justo odio hteh reth  rth rtasfdreg   </li>
+		    <li class="list-group-item">Dapibus ac facilisis int ert rth thgh erqhg h</li>
+		    <li class="list-group-item">Vestibulum at eros ert rth rthr tfdhgdg</li>
+		  </ul>
+		  <div class="card-block">
+			<a href="#" class="btn btn-primary">GO</a>
+		  </div>
+		</div>
+
+		<div class="card text-center" style="width: 20rem;margin: 10px;">
+			  <div class="card-header">
+			    Featured
+			  </div>
+		  <div class="card-block">
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		  </div>
+		  <div class="card-block">
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		  </div>
+		  <ul class="list-group list-group-flush">
+		    <li class="list-group-item">Cras justo odiodbgxxzb strehgrthrth</li>
+		    <li class="list-group-item">Dapibus ac facilisis in terhearthgert heth eth</li>
+		    <li class="list-group-item">Vestibulum at erosh etherth th therter hyehehte </li>
+		    <li class="list-group-item">Cras justo odio hteh reth  rth rtasfdreg   </li>
+		    <li class="list-group-item">Dapibus ac facilisis int ert rth thgh erqhg h</li>
+		    <li class="list-group-item">Vestibulum at eros ert rth rthr tfdhgdg</li>
+		  </ul>
+		  <div class="card-block">
+			<a href="#" class="btn btn-primary">GO</a>
+		  </div>
+		</div>
+
+		<div class="card text-center" style="width: 20rem;margin: 10px;">
+			  <div class="card-header">
+			    Featured
+			  </div>
+		  <div class="card-block">
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		  </div>
+		  <div class="card-block">
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		  </div>
+		  <ul class="list-group list-group-flush">
+		    <li class="list-group-item">Cras justo odiodbgxxzb strehgrthrth</li>
+		    <li class="list-group-item">Dapibus ac facilisis in terhearthgert heth eth</li>
+		    <li class="list-group-item">Vestibulum at erosh etherth th therter hyehehte </li>
+		    <li class="list-group-item">Cras justo odio hteh reth  rth rtasfdreg   </li>
+		    <li class="list-group-item">Dapibus ac facilisis int ert rth thgh erqhg h</li>
+		    <li class="list-group-item">Vestibulum at eros ert rth rthr tfdhgdg</li>
+		  </ul>
+		  <div class="card-block">
+			<a href="#" class="btn btn-primary">GO</a>
+		  </div>
+		</div>
+	</div>
+</div>
+</div>
+<?php
+include('footer.html');
+?>
